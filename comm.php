a309f86810fcb94ebaad09786acc61b7e13954b8
Aarupa <aartilahane013@gmail.com>
2024-02-05 19:37:10 +0530

    first commit

---
+++ comm.php
@@ -0,0 +1,30 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Communication Skill</h2>
+  </div>
+	 
+  <form method="post" action="comm.php">
+  	<?php include('errors.php'); ?>
+      <h3>Select Your Field:-</h3>
+  	<p>
+  		Importance of Comunication: &#160<a href="https://youtu.be/X3Fz_Gu5WUE">click here</a>
+  	</p><br>
+      <p>
+  		English Grammer: &#160<a href="https://youtube.com/playlist?list=PLoZP2WsNfBSGl1lp9t5wTJXHoww8BvGhq">click here</a>
+  	</p><br>
+      <p>
+  		Communication class: &#160<a href="https://youtube.com/playlist?list=PLOaeOd121eBEEWP14TYgSnFsvaTIjPD22">click here</a>
+  	</p><br>
+      <p>
+  		Speak Fluent in one month: &#160<a href="https://youtu.be/og2MHq_dqo0">click here</a>
+  	</p><br>
+  </form>
+</body>
+</html>
